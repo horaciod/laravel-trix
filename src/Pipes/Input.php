951470d0b0f1ca68c513a27ccfebcc3806e6033a
Horacio Degiorgi <horaciod@gmail.com>
2020-06-24 10:11:14 -0300

    recover from model in case of empy in the value from trix tables.

--- src/Pipes/Input.php
+++ src/Pipes/Input.php
@@ -9,7 +9,15 @@ class Input
     public function handle(LaravelTrix $trix, \Closure $next)
     {
         $html = is_object($trix->model) && $trix->model->exists ? htmlspecialchars(optional($trix->model->trixRichText()->where('field', $trix->config['field'])->first())->content, ENT_QUOTES) : '';
+        // in case of empty value  recover from model
+        if ($html == '') {
+            // recovery from model
+            $field = $trix->config['field'];
+            if ($trix->model->$field) {
+                $html = $trix->model->$field ;
+            }
 
+        }
         $trix->html .= "<input id='{$trix->config['id']}' value='{$html}' name='{$trix->loweredModelName}-trixFields[{$trix->config['field']}]' type='hidden'>";
 
         return $next($trix);
